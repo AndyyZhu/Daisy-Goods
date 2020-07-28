72ce07b3c69be8cb44cca0316c27359fc5007b78
joeym-09 <57976341+joeym-09@users.noreply.github.com>
2020-07-28 19:11:42 -0400

    commit

--- preview.hasthemes.com/data.js
+++
@@ -1,5 +0,0 @@
-
-var data = '[' +
-    '{ "name":"John" , "price":"Doe", "img": "images/product/1.png" },' +
-    '{ "name":"John1" , "price":"Doe1", "img": "images/product/2.png" },' +
-    '{ "name":"John2" , "price":"Doe2", "img": "images/product/3.png" }]';
